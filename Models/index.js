f4164204ed1f80cd6030145d9ecff640cd0dc34f
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-01 16:43:02 -0400

    setup thoughts js. have to copy paste this to the corerct file

---
+++ Models/index.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types} = require("mongoose");
+const moment = require("moment");
+
+const reactionSchema = new Schema(
+    {
+        reactionText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format("MM DD, YYY hh:mm a"),
+        },
+    },
+    {
+        toJSON: {
+            type: String,
+            getters: true,
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            minlength: 1,
+            maxlength: 280,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format("MM DD, YYY hh:mm a"),
+        },
+        reactions: [reactionSchema],
+    },
+    {
+            toJSON: {
+                virutals: true,
+                getters: true,
+            },
+            id: false,
+        }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
