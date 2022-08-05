71e25f94bdfa64626dd3e7edbe58a26406337a0d
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-05 06:02:27 -0400

    added expoer route to user routes that was casuing error

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const {
+    createThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    newReaction,
+    deleteReaction, 
+} = require("../../contollers/thoughts-controllers");
+
+router.route("/").get(getAllThoughts);
+
+router
+    .route("/:id")
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+    router.route("/:userId").post(createThought);
+
+    router.route("/:thoughtId/reactions").post(newReaction);
+    router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+    module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require("../../contollers/users-controllers");
+
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
